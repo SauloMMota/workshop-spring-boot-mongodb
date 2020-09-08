f02164e22428b03a2b152088af76e281f056dd35
Saulo Mendes <saulomendes_br@hotmail.com>
2020-09-08 20:49:10 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/saulomendes/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saulomendes.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
