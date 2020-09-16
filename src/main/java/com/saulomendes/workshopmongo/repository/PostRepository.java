54388666ceb62ca01a40a913a3dc3a7b4158470a
Saulo Mendes <saulomendes_br@hotmail.com>
2020-09-15 21:41:34 -0300

    Consulta simples com @Query

--- src/main/java/com/saulomendes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/saulomendes/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.saulomendes.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.saulomendes.workshopmongo.domain.Post;
@@ -11,4 +12,7 @@ import com.saulomendes.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	//?0 o primeiro parâmetro que vem no método
+	@Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> serachTitle(String text);
 }
